fa7ca0cbdeaea6c78ee6852f21a90eef64f960be
Kate Shadrina <eolshadrina@mail.ru>
2022-07-28 11:53:35 +0300

    feature: any crypto price

--- bot.js
+++ bot.js
@@ -2,32 +2,6 @@
 // import BOT_TOKEN from "./settings.js"
 
 const BOT_TOKEN = "5490133596:AAHPuLc6R9-ccnq2saViXI5_EDO1rS_t12Y"
-// const cryptoList = [
-//     'BTC',
-//     'ETH',
-//     'XRP',
-//     'LTC',
-//     'XLM'
-// ]
-
-// const { Telegraf } = require('telegraf')
-// const CoinGecko = require('coingecko-api');
-// const bot = new Telegraf(BOT_TOKEN) //сюда помещается токен, который дал botFather
-// // const CoinGeckoClient = new CoinGecko();
-// const fetch = require("node-fetch")
-
-
-
-
-// bot.start((ctx) => ctx.reply('Welcome')) //ответ бота на команду /start
-// // bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
-// // bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
-// bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
-
-
-
-// bot.hears('BTC', (ctx) => ctx.reply('BTC'))
-// bot.launch() // запуск бота
 
 const express = require('express')
 const expressApp = express()
@@ -70,19 +44,5 @@ bot.on('message', (msg) => {
     bot.telegram.sendMessage(msg.chat.id, message, {});
   });
 })
-
-
-  bot.command('bitcoin', ctx => {
-    var rate;
-    console.log(ctx.from)
-    axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd`)
-    .then(response => {
-      console.log(response.data)
-      rate = response.data.bitcoin
-      const message = `Hello, today the bitcoin price is ${rate.usd}USD`
-      bot.telegram.sendMessage(ctx.chat.id, message, {
-      })
-    })
-  })
 
 bot.launch()
