b227513a7296dc13a235d8c539e87d522b54c9cd
Kate Shadrina <eolshadrina@mail.ru>
2022-07-28 11:52:16 +0300

    feature: any crypto price

--- bot.js
+++ bot.js
@@ -47,24 +47,30 @@ expressApp.get("/", (req, res) => {
 });
 
 
+//функция для создания url для выгрузки цены криптовалютыcre
+const createUrl = (crypto, currency) => {
+  return `https://api.coingecko.com/api/v3/simple/price?ids=${crypto}&vs_currencies=${currency}`;
+};
+
+//приветствие
 bot.command('start', ctx => {
     console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the Code Capsules telegram bot.\nI respond to /ethereum. Please try it', {
+    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the CryptoCurrenciesPricesBot telegram bot.\nI respond to /ethereum. Please try it', {
     })
   })
-  
-  bot.command('ethereum', ctx => {
-    var rate;
-    console.log(ctx.from)
-    axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`)
-    .then(response => {
-      console.log(response.data)
-      rate = response.data.ethereum
-      const message = `Hello, today the ethereum price is ${rate.usd}USD`
-      bot.telegram.sendMessage(ctx.chat.id, message, {
-      })
-    })
-  })
+
+
+bot.on('message', (msg) => {
+  let rate;
+  // let crypto = msg.message.text.slice(1, msg.message.text.length);
+  let crypto = msg.message.text;
+  axios.get(createUrl(crypto, "usd")).then((response) => {
+    rate = response.data[crypto];
+    const message = `The ${crypto} price is ${rate.usd}USD`;
+    bot.telegram.sendMessage(msg.chat.id, message, {});
+  });
+})
+
 
   bot.command('bitcoin', ctx => {
     var rate;
